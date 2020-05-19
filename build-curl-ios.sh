e153ba070858dc34df989c22e2eaf5e197d12df9
Roman Kutashenko <kutashenko@gmail.com>
2020-05-19 11:26:13 +0300

    Build only static libcurl for iOS

--- build-curl-ios.sh
+++ build-curl-ios.sh
@@ -146,14 +146,14 @@ else
 fi
 
 # Build dynamic libraryes
-if [ "$IS_SIMULATOR" == "FALSE" ]; then
-  curl_build_ios armv7 armv7 iPhoneOS iPhoneOS 1
-  curl_build_ios armv7s armv7s iPhoneOS iPhoneOS 1
-  curl_build_ios arm64 arm iPhoneOS iPhoneOS 1
-  aggregate_lib
-else
-  curl_build_ios x86_64 x86_64 iPhoneSimulator iPhoneSimulator 1
-fi
+# if [ "$IS_SIMULATOR" == "FALSE" ]; then
+#   curl_build_ios armv7 armv7 iPhoneOS iPhoneOS 1
+#   curl_build_ios armv7s armv7s iPhoneOS iPhoneOS 1
+#   curl_build_ios arm64 arm iPhoneOS iPhoneOS 1
+#   aggregate_lib
+# else
+#   curl_build_ios x86_64 x86_64 iPhoneSimulator iPhoneSimulator 1
+# fi
 
 copy_headers
 install_to_dest
